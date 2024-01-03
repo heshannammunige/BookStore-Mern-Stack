0778e648dfe6b393f027a91d26353d078b728957
heshannammunige <nammunigeheshan@gmail.com>
2024-01-03 08:24:36 +0530

    my second commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express, { request, response } from "express";
+import { PORT,mongoDBURL } from "./config.js";
 
 
 const app = express();
+
+app.get('/',(request,response)=>{
+    console.log(request);
+    return response.status(234).send('welcome');
+});
 
 app.listen(PORT,()=>{
     console.log(`App is working and listening to port: ${PORT}`);
