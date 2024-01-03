6bf7465e9494e6ee2efd1f924cef08d6750ac96e
heshannammunige <nammunigeheshan@gmail.com>
2024-01-03 08:56:21 +0530

    book model

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express, { request, response } from "express";
 import { PORT,mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 
 const app = express();
@@ -9,6 +10,14 @@ app.get('/',(request,response)=>{
     return response.status(234).send('welcome');
 });
 
-app.listen(PORT,()=>{
-    console.log(`App is working and listening to port: ${PORT}`);
-});
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log("successfullt=y connected to database");
+        app.listen(PORT,()=>{
+            console.log(`App is working and listening to port: ${PORT}`);
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
